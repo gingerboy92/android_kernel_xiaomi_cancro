b282fbaf11c81e376cc2c90b3e73652cf2d48a2b
arnabbiswasalsodeep <arnabbiswasalsodeep@gmail.com>
2015-06-13 19:23:14 +0530

    Build script to its final form
    	modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-MODULES_DIR="../zip/modules"
+ZIP_DIR="../zip"
+ZIMAGE="arch/arm/boot/zImage"
 BUILD_START=$(date +"%s")
-echo "Removing extra backup files"
+echo "Removing extra files"
 find . -type f -name '*~' | xargs -n 1 rm
+rm $ZIMAGE
 echo "Removing Present Modules"
-rm $MODULES_DIR/*
+rm $ZIP_DIR/modules/*
+echo "Building Kernel"
+make -j2
 echo "Stripping  Modules For Size"
 find . -type f -name '*.ko' | xargs -n 1 /home/arnab/android/arm-eabi-6.0/bin/arm-eabi-objcopy --strip-unneeded
-echo "Copying Modules to zip Directory"
-find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+echo "Copying Files to zip Directory"
+cp $ZIMAGE $ZIP_DIR
+find . -name '*.ko' -exec cp {} $ZIP_DIR/modules/ \;
+BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
 echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
