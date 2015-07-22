76e5025867937366223792ef94c5accd48610c64
Arnab Biswas <arnabbiswasalsodeep@gmail.com>
2015-07-22 10:57:40 +0530

    Set it all up
    	modified:   Makefile
    	new file:   arch/arm/configs/stellar_defconfig
    	new file:   build.sh
    	modified:   scripts/mkcompile_h

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ZIP_DIR="zip"
+ZIMAGE="arch/arm/boot/zImage"
+COMPILER="../arm-eabi-5.1/bin/arm-eabi"
+ZIP_NAME=StellAR_ivan_$(date +%F)
+BUILD_START=$(date +"%s")
+echo "Removing Present files"
+find . -type f -name '*~' | xargs -n 1 rm
+rm $ZIMAGE
+rm ../$ZIP_DIR/modules/*
+rm ../$ZIP_DIR/zImage
+ccache -C
+echo "Building Kernel"
+make -j4 CROSS_COMPILE=$COMPILER-
+echo "Stripping  Modules For Size"
+find . -type f -name '*.ko' | xargs -n 1 $COMPILER-objcopy --strip-unneeded
+echo "Copying Files to zip Directory"
+cp $ZIMAGE ../$ZIP_DIR
+find . -name '*.ko' -exec cp {} ../$ZIP_DIR/modules/ \;
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+cd ../$ZIP_DIR
+find . -name '*.zip' -exec mv {} output \;
+zip -r9 --exclude=*output* $ZIP_NAME.zip *
